4332803094c47bc2e13c799492727192cd2e0615
lzl0125 <lianglun0125@gmail.com>
2022-12-12 11:33:21 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,10 @@ public class Calculator {
   public int add(int x, int y) {
     return x + y;
   }
+  public int subtract(int x,int y){
+        return x-y;
+    }
+  public int multiplication(int x,int y){
+        return x*y;
+    }
 }
